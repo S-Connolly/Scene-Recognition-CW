e8ac678ddd278051214a027e2112878755290b09
sc4g18 <sc4g18@soton.ac.uk>
2021-01-03 18:16:11 +0000

    Added K Nearest Neighbour classifier class stub

---
+++ src/main/java/uk/ac/soton/ecs/KNN.java
@@ -0,0 +1,25 @@
+package uk.ac.soton.ecs;
+
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KNN
+{
+	public KNN(int k)
+	{
+
+	}
+
+	public void train(GroupedDataset trainingDataset)
+	{
+
+	}
+
+	public Map<String, String> test(ListDataset testingDataset)
+	{
+		return new HashMap<>();
+	}
+}
