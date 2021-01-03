eb2566882aa27dcbbe7a831038710f3e20189f87
sc4g18 <sc4g18@soton.ac.uk>
2021-01-03 19:01:46 +0000

    Added a method to write the predictions to a txt file in the required format.
    Also changed some methods to private that were previously public

--- src/main/java/uk/ac/soton/ecs/Main.java
+++ src/main/java/uk/ac/soton/ecs/Main.java
@@ -7,13 +7,18 @@ import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
+import org.openrdf.query.algebra.Str;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Main
 {
-    public static void main( String[] args ) throws FileSystemException
+    public static void main( String[] args ) throws IOException
     {
         // Load datasets
         GroupedDataset trainingData = loadTrainingData();
@@ -22,7 +27,8 @@ public class Main
         // Run #1 (K Nearest Neighbour)
         KNN knn = new KNN(10);
         knn.train(trainingData);
-        Map<String, String> result = knn.test(testingData);
+        Map<String, String> knnPredictions = knn.test(testingData);
+        makePredictionFile("run1", knnPredictions);
     }
 
     /**
@@ -30,7 +36,7 @@ public class Main
      * @return VFSGroupDataset of training images
      * @throws FileSystemException If there is an error loading any of the image files
      */
-    public static VFSGroupDataset<FImage> loadTrainingData() throws FileSystemException
+    private static VFSGroupDataset<FImage> loadTrainingData() throws FileSystemException
     {
         String path = new File("training").getAbsolutePath();
         return new VFSGroupDataset<>(path, ImageUtilities.FIMAGE_READER);
@@ -41,9 +47,27 @@ public class Main
      * @return VFSLListDataset of testing images
      * @throws FileSystemException If there is an error loading any of the image files
      */
-    public static VFSListDataset<FImage> loadTestingData() throws FileSystemException
+    private static VFSListDataset<FImage> loadTestingData() throws FileSystemException
     {
         String path = new File("testing").getAbsolutePath();
         return new VFSListDataset<>(path, ImageUtilities.FIMAGE_READER);
+    }
+
+    /**
+     * Writes the predictions to a txt file in the required format
+     * @param fileName Name of the file to create (.txt extension added automatically)
+     * @param predictions The map of image file to class predictions
+     * @throws IOException If there is an error writing to the file
+     */
+    private static void makePredictionFile(String fileName, Map<String, String> predictions) throws IOException
+    {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName + ".txt"));
+
+        for(Map.Entry<String, String> pair : predictions.entrySet())
+        {
+            writer.write(pair.getKey() + " " + pair.toString());
+        }
+
+        writer.close();
     }
 }
