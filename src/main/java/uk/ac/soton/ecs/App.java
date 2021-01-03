ab7f70ae61135fd683b93597c335d6f27ba80f94
viliusjas <viliusjaseliunas@gmail.com>
2021-01-03 17:17:52 +0000

    First push attempt + .gitignore

--- src/main/java/uk/ac/soton/ecs/App.java
+++ src/main/java/uk/ac/soton/ecs/App.java
@@ -7,25 +7,24 @@ import org.openimaj.image.colour.RGBColour;
 import org.openimaj.image.processing.convolution.FGaussianConvolve;
 import org.openimaj.image.typography.hershey.HersheyFont;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * OpenIMAJ Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
-    	//Create an image
-        MBFImage image = new MBFImage(320,70, ColourSpace.RGB);
 
-        //Fill the image with white
-        image.fill(RGBColour.WHITE);
-        		        
-        //Render some test into the image
-        image.drawText("Hello World", 10, 60, HersheyFont.CURSIVE, 50, RGBColour.BLACK);
+        String trainingFolderPath = new File("training").getAbsolutePath();
+        String testingFolderPath = new File("testing").getAbsolutePath();
 
-        //Apply a Gaussian blur
-        image.processInplace(new FGaussianConvolve(2f));
-        
-        //Display the image
-        DisplayUtilities.display(image);
+        List<String> labels = Arrays.asList("bedroom", "Coast", "Forest",
+                "Highway", "industrial", "Insidecity", "kitchen",
+                "livingroom", "Mountain", "Office", "OpenCountry",
+                "store", "Street", "Suburb", "TallBuilding");
+
     }
 }
